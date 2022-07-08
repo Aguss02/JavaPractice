fd7863d34077dbda0116dfdfbde4bb1a354c0d1e
Aguss02 <tosi.agustina02@gmail.com>
2022-07-08 11:21:34 -0300

    second commit

---
+++ 3. POO Java Playground/DH-Clase12Docente/src/PrincipalMain.java
@@ -0,0 +1,16 @@
+
+public class PrincipalMain {
+
+	public static void main(String[] args) {
+		CajaAhorro ca = new CajaAhorro();
+		ca.setSaldo(100);
+		ca.cobrarIntereses();
+
+		System.out.println(ca.informarSaldo());
+
+	}
+
+
+
+
+}
